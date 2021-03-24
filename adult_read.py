e7b33c0135f412625587684cc8382ba3482cd661
yzeng58 <yzeng58@wisc.edu>
2021-03-23 23:52:59 -0500

    ignore update

--- adult_read.py
+++ adult_read.py
@@ -2,6 +2,11 @@ import os
 import pandas as pd
 import numpy as np
 
+from torchvision import transforms
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import torch
+
 def process_adult(filename):
     header = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status',
                 'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss',
@@ -36,8 +41,16 @@ def process_adult(filename):
     adult[pred_var] = adult[pred_var].astype('category').cat.codes
     return adult
 
-adult = process_adult('adult.data')
-test = process_adult('adult.test')
+class LoadData(Dataset):
+    def __init__(self, df, pred_var):
+        self.y = df[pred_var].values
+        self.x = df.drop(pred_var, axis = 1).values
+    
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+    
+    def __len__(self):
+        return self.y.shape[0]
 
 if __name__ == "adult_read":
     adult = process_adult('adult.data')
@@ -56,5 +69,26 @@ if __name__ == "adult_read":
 
     client2 = adult.iloc[client2_idx]
     client2 = client2.reset_index(drop=True)
-    
+
     clients = [client1, client2]
+
+    ### torch preparation ###
+    ################## MODEL SETTING ########################
+    BATCH_SIZE = 128
+    NUM_WORKERS = 4
+    RANDOM_SEED = 123
+    DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    client1_dataset = LoadData(client1, 'salary')
+    client1_loader = DataLoader(dataset = client1_dataset,
+                                batch_size = BATCH_SIZE,
+                                num_workers = NUM_WORKERS)
+
+    client2_dataset = LoadData(client2, 'salary')
+    client2_loader = DataLoader(dataset = client2_dataset,
+                                batch_size = BATCH_SIZE,
+                                num_workers = NUM_WORKERS)
+
+    test_dataset = LoadData(test, 'salary')
+    test_loader = DataLoader(dataset = test_dataset,
+                                batch_size = BATCH_SIZE,
+                                num_workers = NUM_WORKERS)
