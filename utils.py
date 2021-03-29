b8cfc47cbb765f30671b1abf24c45d7be7ab96ae
yzeng58 <yzeng58@wisc.edu>
2021-03-29 18:45:18 -0500

    FedAvg Pytorch

--- utils.py
+++ utils.py
@@ -47,14 +47,16 @@ class LoadData(Dataset):
         self.x = df.drop(pred_var, axis = 1).values
     
     def __getitem__(self, index):
-        return self.x[index], self.y[index]
+        return torch.tensor(self.x[index]), torch.tensor(self.y[index])
     
     def __len__(self):
         return self.y.shape[0]
 
-if __name__ == "adult_read":
+if __name__ == "utils":
     adult = process_adult('adult.data')
     test = process_adult('adult.test')
+    test['native-country_ Holand-Netherlands'] = 0
+    test = test[adult.columns]
 
     np.random.seed(1)
     adult_private_idx = adult[adult['workclass_ Private'] == 1].index
@@ -63,21 +65,30 @@ if __name__ == "adult_read":
     client1_idx = np.concatenate((np.random.choice(adult_private_idx, int(.8*len(adult_private_idx)), replace = False),
                                     np.random.choice(adult_others_idx, int(.2*len(adult_others_idx)), replace = False)))
     client2_idx = np.array(list(set(adult.index) - set(client1_idx)))
-
-    client1 = adult.iloc[client1_idx]
+    clients_idx = [client1_idx, client2_idx]
+    client1 = adult.iloc[clients_idx[0]]
     client1 = client1.reset_index(drop=True)
 
-    client2 = adult.iloc[client2_idx]
+    client2 = adult.iloc[clients_idx[1]]
     client2 = client2.reset_index(drop=True)
 
     clients = [client1, client2]
 
-    ### torch preparation ###
+    ################## torch preparation ##################
+
     ################## MODEL SETTING ########################
     BATCH_SIZE = 128
     NUM_WORKERS = 4
     RANDOM_SEED = 123
+    NUM_EPOCHS = 20 
+    LEARNING_RATE = 0.05
+    FRAC = 1 # the fraction of clients
+    NUM_CLIENTS = 2
     DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    OPTIMIZER = 'sgd'
+    LOCAL_EPOCHS = 10
+    NUM_FEATURES = len(adult.columns)-1
+    #########################################################
     client1_dataset = LoadData(client1, 'salary')
     client1_loader = DataLoader(dataset = client1_dataset,
                                 batch_size = BATCH_SIZE,
@@ -92,3 +103,26 @@ if __name__ == "adult_read":
     test_loader = DataLoader(dataset = test_dataset,
                                 batch_size = BATCH_SIZE,
                                 num_workers = NUM_WORKERS)
+    
+    train_dataset = LoadData(adult, 'salary')
+    train_loader = DataLoader(dataset = train_dataset,
+                            batch_size = BATCH_SIZE,
+                            num_workers = NUM_WORKERS)
+
+    torch.manual_seed(0)
+
+    # test whether dataset can be loaded correctly
+    num_epochs = 2
+    for epoch in range(num_epochs):
+
+        for batch_idx, (x, y) in enumerate(client1_loader):
+            
+            print('Epoch:', epoch+1, end='')
+            print(' | Batch index:', batch_idx, end='')
+            print(' | Batch size:', y.size()[0])
+            
+            x = x.to(DEVICE)
+            y = y.to(DEVICE)
+            
+            print('break minibatch for-loop')
+            break
