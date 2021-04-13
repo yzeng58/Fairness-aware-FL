bb313be8cd2f3cb7e0532b06d5725a98f698aae3
yzeng58 <yzeng58@wisc.edu>
2021-04-13 14:29:34 -0500

    removed misleading testloader

--- load_adult.py
+++ load_adult.py
@@ -58,7 +58,7 @@ class LoadData(Dataset):
    
 if __name__ == "load_adult":
     adult = process_adult('adult.data')
-    test = process_adult('adult.test')
+    test = process_adult('adult.test') # the distribution is very different from training distribution
     test['native-country_ Holand-Netherlands'] = 0
     test = test[adult.columns]
 
